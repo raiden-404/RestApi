e3d9d27eea36f9df2de8fd397622de931bd867c2
raiden-404 <patelyuvrajsingh789@gmail.com>
2024-12-19 16:04:35 +0530

    Add all CRUD ops and proper HttpStatus

--- book_api/src/main/java/com/api/bookmanagment/bookEntitiesAndMethods/BookServiceProvider.java
+++ book_api/src/main/java/com/api/bookmanagment/bookEntitiesAndMethods/BookServiceProvider.java
@@ -1,32 +1,65 @@
 package com.api.bookmanagment.bookEntitiesAndMethods;
+
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
 @Component
 public class BookServiceProvider {
     @Autowired
     private BookRepository bookRepository;
 
-    //Method to return all book in DB
-    public List<Book> getAllBooks(){
+    // Method to return all book in DB
+    public List<Book> getAllBooks() {
         return bookRepository.findAll();
     }
 
-    //Method to return book by ID
-    public Book getBookById(int id){
-        return bookRepository.findById(id).orElse(null);
+    // Method to return book by ID
+    public Book getBookById(int id) {
+        Book book = bookRepository.findById(id).orElse(null);
+        if(book == null ){
+            Book bookNotFound = new Book(-1,"-1","-1");
+            return bookNotFound;
+        }
+        return book;
     }
 
-    //Method to add Book
-    public String setBook(DTO dto){
+    // Method to add Book
+    public String setBook(DTO dto) {
         Book book = new Book();
         book.setName(dto.getName());
         book.setAuthor(dto.getAuthor());
-        Book addedBook = bookRepository.save(book);
-        if(addedBook.getName().equals(book.getName())  &&  addedBook.getAuthor().equals(book.getAuthor())){
-            return "Book Added Successfully!!";
-        }else{
-            return "Error occured to Add";
+        try {
+            Book addedBook = bookRepository.save(book);
+            if (addedBook.getName().equals(book.getName()) && addedBook.getAuthor().equals(book.getAuthor())) {
+                return "Book Added Successfully!!";
+            } else {
+                return "Error occured to Add";
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+        return "Error";
     }
+
+    // Delete Book by its ID
+    public String deleteBookById(int id) {
+        try {
+            String bookName = bookRepository.findById(id).orElse(null).getName();
+            bookRepository.deleteById(id);
+            return bookName;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "Error";
+    }
+
+    // Update Book by its ID
+    public String updateBookById(DTO bookDto, int id) {
+        Book book = bookRepository.findById(id).orElse(null);
+        book.setName(bookDto.getName());
+        book.setAuthor(bookDto.getAuthor());
+        return bookRepository.save(book).getName();
+    }
+
 }
--- book_api/src/main/java/com/api/bookmanagment/controllers/BookController.java
+++ book_api/src/main/java/com/api/bookmanagment/controllers/BookController.java
@@ -5,11 +5,19 @@ import com.api.bookmanagment.bookEntitiesAndMethods.Book;
 import com.api.bookmanagment.bookEntitiesAndMethods.BookServiceProvider;
 import com.api.bookmanagment.bookEntitiesAndMethods.DTO;
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 
 // @Controller
@@ -20,18 +28,47 @@ public class BookController {
     // @RequestMapping(value = "/book", method=RequestMethod.GET)
     //Sending all books to the server
     @GetMapping("/book")
-    public List<Book> getAllBooks() {
-        return bookServiceProvider.getAllBooks();
+    public ResponseEntity<List<Book>> getAllBooks() {
+        if(bookServiceProvider.getAllBooks().size() <= 0){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        return ResponseEntity.of(Optional.ofNullable(bookServiceProvider.getAllBooks()));
     }
+
     //Sending single book to the server by his ID
     @GetMapping("/book/{id}")
-    public Book getBookById(@PathVariable("id") int id){
-        return bookServiceProvider.getBookById(id);
+    public ResponseEntity<Book> getBookById(@PathVariable("id") int id){
+        if(bookServiceProvider.getBookById(id) == null){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        return ResponseEntity.of(Optional.ofNullable(bookServiceProvider.getBookById(id)));
     }
     
     //Fetching Book from server and Adding to the Database
     @PostMapping("/book")
-    public String setBook(@RequestBody DTO dto) {
-        return bookServiceProvider.setBook(dto);
+    public ResponseEntity<String> setBook(@RequestBody DTO dto) {
+        if(dto.getName() == null || dto.getAuthor() == null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+        if(bookServiceProvider.setBook(dto).equals("Book Added Successfully!!")){
+            return ResponseEntity.ok("Book Added Successfully!!");
+        }
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+    }
+
+    //Deleting Book by ID
+    @DeleteMapping("/book/delete/{id}")
+    public ResponseEntity<String> deleteBookById(@PathVariable("id") int id){
+        String name = bookServiceProvider.getBookById(id).getName();
+        if(bookServiceProvider.deleteBookById(id).equals(name)){
+            return ResponseEntity.ok("Book: \""+name+"\" is Successfully Deleted!!");
+        }
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    //Update Book by ID
+    @PutMapping("book/{id}")
+    public String updateBookById(@RequestBody DTO bookDto,@PathVariable int id) {
+        return "Book: \""+bookServiceProvider.updateBookById(bookDto, id)+"\" is Successfully Updated!!";
     }
 }
